4262845e817963c7d04b3fa877482e47b4858d5a
lalkmim@gmail.com <>
2012-06-29 16:30:05 +0000

    teste contador

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var i = 0;
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(110);
+  response.send((i++) + '');
 });
 
 var port = process.env.PORT || 5000;
