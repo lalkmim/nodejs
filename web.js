5a3974ce943ac045604d5f06ba3418ea52003f3d
lalkmim@gmail.com <>
2012-06-29 02:13:58 +0000

    teste

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var i = 0;
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(i++);
+  response.send('Teste teste');
 });
 
 var port = process.env.PORT || 5000;
