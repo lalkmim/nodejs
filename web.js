ef595162b273c1875a327b9fdc477bbaf80ada3b
lalkmim@gmail.com <>
2012-06-29 20:09:05 +0000

    Correção da porta utilizada

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var app = require('express').createServer();
 
 var io = require('socket.io').listen(app);
 
-app.listen(80);
+app.listen(8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
