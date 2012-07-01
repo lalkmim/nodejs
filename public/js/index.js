835f7240b2e739f37c8c9532b1025baa2936e748
Leonardo Alkmim <lalkmim@gmail.com>
2012-07-01 11:19:51 -0300

    Separacao dos arquivos .js

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+var socket = null;
+      
+$(document).ready(function() {
+	$('#div_texto').css('display', 'none');
+	$('#div_conectar').css('display', 'block');
+	
+	$('#but_ok').live('click', function() {
+		socket.emit('chat', { 
+			msg: $('#texto').val(),
+			dataHora: formatHour(new Date()),
+			autor: $('#nome').val() });
+	});
+
+	$('#but_conectar').live('click', function() {
+		$('#but_conectar').attr('disabled', 'disabled');
+		$('#but_conectar').attr('value', 'Conectando...');
+		
+		socket = io.connect(window.location.href);
+		socket.on('chat', function (data) {
+			$('#div_conectar').css('display', 'none');
+			$('#div_texto').css('display', 'block');
+		
+			console.log(data);
+			$('#chat').append('<br/>[');
+			$('#chat').append(data.dataHora);
+			$('#chat').append('] ');
+			$('#chat').append(data.autor);
+			$('#chat').append(': ');
+			$('#chat').append(data.msg);
+		});
+	});
+});
